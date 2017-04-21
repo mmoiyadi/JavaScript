2904c2b20c39fe3e6e6ae25065030d432a440297
Mohammed Moiyadi <mmoiyadi@realization.com>
2017-04-21 14:53:39 +0530

    mmeoryMapGame

---
+++ memoryMatch.js
@@ -0,0 +1,88 @@
+var count = 0;
+var timerInterval;
+
+
+
+timerListener = function(){
+	if(timerInterval)
+		clearInterval(timerInterval);
+	count = 0;
+	timerInterval = setInterval(function(){
+	count++;
+	document.getElementById("timer").innerHTML = "Time Elapsed:" + count;
+	},1000);
+	start();
+}
+
+document.getElementById("restart").addEventListener("click",timerListener);
+
+compare = function(cell1, cell2){
+	var id1 = cell1.getAttribute("id");
+	var id2 = cell2.getAttribute("id");
+	if(id1 == id2)
+		return false;
+	if(numbers[id1] == numbers[id2])
+		return true;
+	else return false;
+}
+
+reveal = function(cell){
+	cell.style.background = "green";
+	var id = cell.getAttribute("id");
+	cell.innerText = numbers[id];
+}
+
+hide = function(cell){
+	cell.style.background = "purple";
+	cell.innerText = "";
+}
+
+clickHandler = function(){
+		 console.log(this);
+		 reveal(this);
+		 if(pairing){
+			 if(compare(this, pairing)){
+				reveal(pairing);
+				reveal(this);
+				pairing = null;
+				paired++;
+				if(paired == 4){
+					alert("You won the game in " + count + " seconds");
+					paired=0;
+					if(timerInterval)
+						clearInterval(timerInterval);
+				}
+				return;
+			 }
+			 else{
+				 pairing = null;
+			 }
+		 }
+		 else{
+			 pairing = this;
+		 }
+		 setTimeout(function(){
+			 hide(this);
+		 }.bind(this),500);
+}
+
+var pairing=null;
+var numbers = [];
+var paired = 0;
+
+start = function(populate){
+	var table = document.getElementById("gridTable");
+	for (var i = 0, row; row = table.rows[i]; i++) {
+	   for (var j = 0, col; col = row.cells[j]; j++) {
+		 var id = col.getAttribute("id");
+		 if(populate){
+			numbers[id] = col.innerText;
+			col.addEventListener("click",clickHandler);
+		 }
+		 hide(col);
+	   }  
+	}
+}
+
+start(true);
+timerListener();
